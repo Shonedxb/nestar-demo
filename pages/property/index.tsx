5221f0d8a3dfd23b654879024abae96b48606e0b
“Shonedxb” <firdavs.cra@gmail.com>
2025-07-13 16:12:28 +0400

    feat: Organize and test pages routing system

---
+++ pages/property/index.tsx
@@ -0,0 +1,10 @@
+const PropertyList = () => {
+  return (
+    <div>
+      <h1>Property List</h1>
+      <p>Here you can find a list of properties.</p>
+    </div>
+  );
+};
+
+export default PropertyList;
