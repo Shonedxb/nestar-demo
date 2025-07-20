8b762f2185346e49b80ffea8d15c3581b7b744df
“Shonedxb” <firdavs.cra@gmail.com>
2025-07-20 18:32:25 +0400

    feat: Integrated SASS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useState } from "react";
 import { light } from "../scss/MaterialTheme";
 import { CssBaseline } from "@mui/material";
+import "../scss/app.scss";
 
 
 export default function App({ Component, pageProps }: AppProps) {
